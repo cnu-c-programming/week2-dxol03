dd9a507e13365601386e8fbd66b0680a87f2d1ad
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-03-14 17:14:09 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int n, i;
+    int is_prime = 1; 
+
+    if (scanf("%d", &n) != 1) return 1;
+
+    if (n <= 1) {
+        is_prime = 0;
+    } else {
+        for (i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                is_prime = 0; 
+                break;        
+            }
+        }
+    }
+
+    if (is_prime) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
 
     return 0;
 }
-
