23eb94730e044c895889a36560114e173f9bf2d8
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-03-14 17:09:35 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int num1, num2;
+    char op;
+
+    if (scanf("%d %d %c", &num1, &num2, &op) != 3) {
+        return 1;
+    }
+
+    if (op == '+') {
+        printf("%d\n", num1 + num2);
+    } else if (op == '-') {
+        printf("%d\n", num1 - num2);
+    } else if (op == '*') {
+        printf("%d\n", num1 * num2);
+    } else if (op == '/') {
+        if (num2 != 0) {
+            printf("%d\n", num1 / num2); // 정수 나눗셈
+        } else {
+            printf("0으로 나눌 수 없습니다.\n");
+        }
+    }
 
     return 0;
 }
-
