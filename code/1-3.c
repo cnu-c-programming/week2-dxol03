611197c6e0bfe1dc1f24ef0ec57aa22640258dfd
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-03-14 17:32:10 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    int age;       
-    float gpa;     
-    char grade;    
+int main()
+{
+    int age;
+    char grade;
+    float gpa;
 
+    scanf("%d %c %f", &age, &grade, &gpa);
     
-    scanf("%d %f %c", &age, &gpa, &grade);
-
-    printf("%d %.2f %c\n", age, gpa, grade);
+    printf("%d %c %f\n",age,grade,gpa);
 
     return 0;
+    
 }
