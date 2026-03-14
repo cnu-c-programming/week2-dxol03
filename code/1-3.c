301d851abf5829836a9146228bfec66b77273f1a
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-03-14 16:46:22 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int age;       
+    float gpa;     
+    char grade;    
 
+    
+    scanf("%d %f %c", &age, &gpa, &grade);
+
+    printf("%d %.2f %c\n", age, gpa, grade);
 
     return 0;
 }
